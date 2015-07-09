87223fa55572036d11d9d173076f95e85473fec4
BSick7 <brad.sickles@gmail.com>
2015-07-09 10:42:51 -0400

    Updated example.

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,11 +1,11 @@
 resource "fortycloud_subnet" "subnet1" {
 	name = "subnet1"
 	description = "Subnet 1"
-	subnet = "10.1.0.0/17"
+	subnet = "10.1.0.0/16"
 }
 
 resource "fortycloud_subnet" "subnet2" {
 	name = "subnet2"
 	description = "Subnet 2"
-	subnet = "10.1.0.0/17"
+	subnet = "10.2.0.0/16"
 }
